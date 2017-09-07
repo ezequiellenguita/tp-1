ae21160e47aa105ecc0f0d58174ef7d19084e79c
ezequiellenguita <elgildeeze@gmail.com>
2017-09-07 10:40:23 -0300

    Add files via upload

---
+++ tp-calculadora-master/funciones.c
@@ -0,0 +1,43 @@
+
+void sumar(float a, float b)
+{
+    float resultado = a + b;
+    printf("El resultado es %.2f \n", resultado);
+}
+void restar(float a, float b)
+{
+    float resultado = a - b;
+    printf("El resultado es %.2f \n", resultado);
+}
+void multiplicar(float a, float b)
+{
+    float resultado = a * b;
+    printf("El resultado es %.2f \n", resultado);
+}
+void dividir(float a, float b)
+{
+    if (b != 0)
+    {
+        float resultado = a / b;
+        printf("El resultado es %.2f \n", resultado);
+    }
+    else
+    {
+        printf("Operacion no permitida: Divicion por zero \n");
+    }
+}
+void factorial(float a)
+{
+    int i;
+    float factorial = 1;
+
+    if (a < 0)
+    {
+        printf("Operacion no permitida: Factorial de un numero negativo no existe \n");
+    }
+    for (i = 1; i <= a; i++)
+    {
+        factorial = factorial * i;
+    }
+    printf("El factorial es %.2f \n", factorial);
+}
---
+++ tp-calculadora-master/funciones.h
@@ -0,0 +1,49 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+#include <stdio.h>
+
+/** @brief Realiza la suma de A y B
+ *  y lo muestra en pantalla
+ *
+ *  @param a El primer operando.
+ *  @param b El segundo operando
+ *  @return Void.
+ */
+void sumar(float a, float b);
+
+/** @brief Realiza la resta de A y B
+ *  y lo muestra en pantalla
+ *
+ *  @param a El primer operando.
+ *  @param b El segundo operando
+ *  @return Void.
+ */
+void restar(float a, float b);
+
+/** @brief Realiza la multiplicación de A y B
+ *  y lo muestra en pantalla
+ *
+ *  @param a El primer operando.
+ *  @param b El segundo operando
+ *  @return Void.
+ */
+void multiplicar(float a, float b);
+
+/** @brief Realiza la divicion de A y B
+ *  y lo muestra en pantalla. Valida no realizar division por 0
+ *
+ *  @param a El primer operando.
+ *  @param b El segundo operando
+ *  @return Void.
+ */
+void dividir(float a, float b);
+
+/** @brief Calcula el factorial de A
+ *  y lo muestra en pantalla
+ *
+ *  @param a El numero sobre el cual calcular el factorial
+ *  @return Void.
+ */
+void factorial(float a);
+
+#endif // FUNCIONES_H_INCLUDED
